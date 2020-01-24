56c1d2144690b6ec118217cd142b5c212241eae5
DSInterns <60170220+DSInterns@users.noreply.github.com>
2020-01-24 10:02:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Education',
         'Topic :: System :: Hardware',
         'Topic :: System :: Hardware :: Hardware Drivers'
